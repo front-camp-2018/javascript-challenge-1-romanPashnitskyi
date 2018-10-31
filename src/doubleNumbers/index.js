c1cc9358b7de4024d3ffbd1b63bd399b4750ca97
romanPashnitskyi <bernald_07@meta.ua>
2018-10-31 15:55:19 +0200

    my changes

--- src/doubleNumbers/index.js
+++ src/doubleNumbers/index.js
@@ -1,4 +1,24 @@
 /* eslint-disable-next-line */
 export const doubleNum = num => {
-  // your logic here...
+
+  const array = ('' + num).split('');
+  const arrayLength = array.length;
+  const halfLength = arrayLength / 2;
+
+  if (Number.isInteger(halfLength)) {
+
+    const leftSide = array.slice(0, halfLength);
+    const rightSide = array.slice(leftSide.length, arrayLength);
+    const leftNumber = leftSide.join('');
+    const rightNumber = rightSide.join('');
+
+    if (leftNumber === rightNumber) {
+      return Number.parseInt(leftNumber + rightNumber);
+    } else {
+      return Number.parseInt((leftNumber + rightNumber) * 2);
+    }
+  } else {
+    return num * 2;
+  }
+
 };
--- src/sortByWeight/index.js
+++ src/sortByWeight/index.js
@@ -1,4 +1,27 @@
 /* eslint-disable-next-line */
 export const sortByWeight = arr => {
-  // your logic here...
+
+  const map = {};
+  const order = {};
+  const newArr = [];
+
+  arr.forEach(function (element) {
+    const arrayInside = ('' + element).split('');
+    const item = arrayInside.map(Number);
+    map[element] = item.reduce(add, 0);
+  });
+
+  Object.values(map).forEach(function (key, value) {
+    order[key] = Object.keys(map)[value];
+  });
+
+  Object.values(order).forEach(function (element) {
+    newArr.push(element);
+  });
+
+  function add (a, b) {
+    return a + b;
+  }
+
+  return newArr;
 };
